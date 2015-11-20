cec5dbd21f2c014aec52f98355ed3f5b7c9e0fef
Mikey <mikey@wlan152-255.wlan.wwu.edu>
2015-11-19 19:31:06 -0800

    getting correct output, just need to fix puts statement

--- Reverse Add/ReverseAdd.rb
+++ Reverse Add/ReverseAdd.rb
@@ -2,22 +2,30 @@
 #Codeeval Challange 
 
 
-def main ()
+def main
 	File.open(ARGV[0]).each_line do |line|
 
 		i = 0 
-		number = line
-		reverseadd(number i)
-		puts number
-		puts i
+		number = line.to_i
+		reverseadd(number, i)
+
 	end
+
 end
 
-def reverseadd (number i)
+
+def reverseadd (number, i)
 	
 	while number != number.to_s.reverse.to_i do 
 		number += number.to_s.reverse.to_i
 		i += 1 
 	end
+	puts number
+	puts i
 end
+
+main()
+
+
+
 
