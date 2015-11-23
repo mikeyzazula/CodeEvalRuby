91d9e435aae9c826d4eb9ebc78c5a503b89b6214
Mikey <mikey@wlan152-215.wlan.wwu.edu>
2015-11-23 15:09:01 -0800

    Achieved mess status

--- matrixpath/matrixpath.rb
+++ matrixpath/matrixpath.rb
@@ -1,2 +1,33 @@
 #Michael Zazula
-#Matrix Path Challenge
+#CodeEval Challenge - Min. Path Challenge
+
+def main
+
+	require "Matrix"
+	mat = Matrix[]
+	
+	File.open(ARGV[0]).each_line do |line|
+		if line.to_s.chomp.length != 1
+			addtomatrix(line,mat)
+
+		end
+
+
+	end
+
+	puts mat
+
+end
+
+
+
+def addtomatrix(line, mat)
+	array = line.split(",").map(&:to_i)
+	mat = Matrix.rows(mat.to_a << array)
+
+
+end
+
+
+
+main 
